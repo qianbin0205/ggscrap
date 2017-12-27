4b29e8bd16329fdab8bd88cc99bdd3ed950254ea
unknown <wangys@go-goal.com>
2017-12-27 18:29:12 +0800

    ......

--- runNewsSpider.py
+++ runNewsSpider.py
@@ -4,4 +4,5 @@ from scrapy import cmdline
 # cmdline.execute(['scrapy', 'crawl', 'News_P5w', '-a', 'limit=50', '-a', 'jobId=0L'])
 # cmdline.execute(['scrapy', 'crawl', 'News_Ccstock', '-a', 'limit=50', '-a', 'jobId=0L'])
 # cmdline.execute(['scrapy', 'crawl', 'News_Stcn', '-a', 'limit=50', '-a', 'jobId=0L'])
-cmdline.execute(['scrapy', 'crawl', 'News_Hexun', '-a', 'limit=50', '-a', 'jobId=0L'])
+# cmdline.execute(['scrapy', 'crawl', 'News_Hexun', '-a', 'limit=50', '-a', 'jobId=0L'])
+cmdline.execute(['scrapy', 'crawl', 'News_Jrj', '-a', 'limit=20', '-a', 'jobId=0L'])
