ddf59c8d2caf4ff76e00bcc22e8d85fdb8f6ab6e
zhanghh <zhanghh@go-goal.com>
2018-01-15 20:08:55 +0800

    ......

--- FundNavSpiders/XinPuAsset.py
+++ FundNavSpiders/XinPuAsset.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import json
+from urllib.parse import quote
 from datetime import datetime
+from scrapy import Request
 from GGScrapy.items import GGFundNavItem
 from GGScrapy.ggspider import GGFundNavSpider
 
@@ -10,19 +12,43 @@ class XinPuAssetSpider(GGFundNavSpider):
     name = 'FundNav_XinPuAsset'
     sitename = '信普资产'
     channel = '投顾净值'
-    allowed_domains = ['xinpibao.com']
-    start_urls = ['http://xinpibao.com/company/M11451.html']
+    allowed_domains = ['www.xinpibao.com']
+    start_urls = ['http://www.xinpibao.com/company/M11451.html']
 
     def __init__(self, limit=None, *args, **kwargs):
         super(XinPuAssetSpider, self).__init__(limit, *args, **kwargs)
 
     def start_requests(self):
-        cookies = 'JSESSIONID=8F341B02391F6365A6358DF0D61D093A; Hm_lvt_bd5f74e276d37b338277623405760296=1515745411,1515745617,1515745634,1515977698; td_cookie=11049129; xpbtoken=2pv8hu9n9rmvgg27079h0561telgv; Hm_lpvt_bd5f74e276d37b338277623405760296=1515982751'
+        # yield Request(url='http://www.xinpibao.com/sso/api/users/random', callback=self.parse_pre_login)
+
+        cookies = self.parse_cookies('td_cookie=11049239; JSESSIONID=83E585FFB36F48C49A2227DC43333A81; Hm_lvt_bd5f74e276d37b338277623405760296=1514455130,1516003028,1516003103; xpbtoken=128068db2nlq4fmuhg88rthg9eeoc; Hm_lpvt_bd5f74e276d37b338277623405760296=1516014521')
+        yield Request(url='http://www.xinpibao.com/web/api/idmapping/?id=M11451', cookies=cookies,
+                      callback=self.parse_map)
+
+    def parse_pre_login(self, response):
+        yield Request(url='http://www.xinpibao.com/sso/api/users/login',
+                      method='POST',
+                      headers={'Content-Type': 'application/x-www-form-urlencoded',
+                               'Referer': 'http://www.xinpibao.com/sso/'},
+                      body=b'{"mobile":"13916427906","password":"eef87e7a685b7e5b7e0cc5aa7c58efd9bd5d1b9b3d610bf85e7f63a64cc365b7"}',
+                      callback=self.parse_login)
+
+    def parse_login(self, response):
+        pass
+
+    def parse_map(self, response):
+        flt = '{"managerCompanyId":"' + eval(response.text)['data'] + '"}'
+        flt = quote(flt)
+
         fps = [
             {
-                'url': 'http://xinpibao.com/web/api/funds/computeInfos/?filter=%7B%22managerCompanyId%22%3A%22CP-646100ac-9846-4a4f-bb47-f5bc1683f218%22%7D&from=0',
-                'ref': 'http://xinpibao.com/company/M11451.html',
-                'cookies': cookies
+                'url': 'http://www.xinpibao.com/web/api/funds/computeInfos/?filter=' + flt + '&from=0',
+                'ref': 'http://www.xinpibao.com/company/M11451.html',
+                # 'cookies': 'td_cookie=11049239; JSESSIONID=83E585FFB36F48C49A2227DC43333A81; Hm_lvt_bd5f74e276d37b338277623405760296=1514455130,1516003028,1516003103; xpbtoken=128068db2nlq4fmuhg88rthg9eeoc; Hm_lpvt_bd5f74e276d37b338277623405760296=1516014521'
+                # 'headers': {'Accept': 'application/json, text/javascript, */*; q=0.01',
+                #             'Accept-Encoding': 'gzip, deflate',
+                #             'Accept-Language': 'zh-CN,zh;q=0.9',
+                #             'X-Requested-With': 'XMLHttpRequest'}
             },
         ]
 
@@ -38,7 +64,8 @@ class XinPuAssetSpider(GGFundNavSpider):
             fund_id = fund['id']
             ips.append({
                 'pg': {'page': 0, 'fund_id': fund_id},
-                'url': lambda pg: 'http://xinpibao.com/web/api/funds/' + str(pg['fund_id']) + '/netValues?from=' + str(6 * pg['page']),
+                'url': lambda pg: 'http://xinpibao.com/web/api/funds/' + str(pg['fund_id']) + '/netValues?from=' + str(
+                    6 * pg['page']),
                 'headers': {
                     'X-Requested-With': 'XMLHttpRequest',
                     'Accept': 'application/json, text/javascript, */*; q=0.01',
@@ -69,12 +96,13 @@ class XinPuAssetSpider(GGFundNavSpider):
             item['statistic_date'] = datetime.strptime(statistic_date, '%Y-%m-%d')
 
             item['nav'] = float(row['shareUnitValue']) if row['shareUnitValue'] is not None else None
-            item['added_nav'] = float(row['shareUnitAccumulateValue']) if row['shareUnitAccumulateValue'] is not None else None
+            item['added_nav'] = float(row['shareUnitAccumulateValue']) if row[
+                                                                              'shareUnitAccumulateValue'] is not None else None
 
             yield item
         total = json.loads(response.text)['total']
         pg = response.meta['pg']
-        if 6*pg['page'] < total:
+        if 6 * pg['page'] < total:
             pg['page'] += 1
             ips.append({
                 'pg': pg,
