7c4ebf7e30c83cfe7a4458eed13979d33c2f3082
unknown <wangys@go-goal.com>
2018-01-26 13:50:37 +0800

    ......

--- run_wys.py
+++ run_wys.py
@@ -3,7 +3,7 @@ from scrapy import cmdline
 # cmdline.execute(['scrapy', 'crawl', 'FundNav_JinguTrust', '-a', 'jobId=0L'])
 # cmdline.execute(['scrapy', 'crawl', 'FundNav_GuangdaBank', '-a', 'jobId=0L'])
 # cmdline.execute(['scrapy', 'crawl', 'FundNav_WuhanWeixinFund', '-a', 'jobId=0L'])
-cmdline.execute(['scrapy', 'crawl', 'FundNav_HainingShibeiInvset', '-a', 'jobId=0L'])
+cmdline.execute(['scrapy', 'crawl', 'FundNav_TianhesichuangInvset', '-a', 'jobId=0L'])
 # cmdline.execute(['scrapy', 'crawl', 'FundNav_XrzFund', '-a', 'jobId=0L'])
 # cmdline.execute(['scrapy', 'crawl', 'FundNav_BaoyinCyInvset', '-a', 'jobId=0L'])
 # cmdline.execute(['scrapy', 'crawl', 'FundNav_XZeastmoney', '-a', 'jobId=0L'])
