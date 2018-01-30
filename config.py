0ce4db04af51a9cb386b5386fc711a2729ca383c
zhanghh <zhanghh@go-goal.com>
2018-01-30 21:24:44 +0800

    ......

--- config.py
+++ config.py
@@ -1,3 +1,19 @@
+SPIDER_MODULES = ['spiders', 'NewsSpiders', 'FundNavSpiders', 'FundNoticeSpiders', 'InteractionSpiders']
+
+DEFAULT_REQUEST_HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'
+}
+
+proxy = 'http://111.13.111.184:80'
+
+ufile = {
+    'public_key': 'ucloudgcqin@go-goal.com13648682571239575500',
+    'private_key': '27f435a8c39f515b01a3db66acbdd7ef9b37d16c',
+    'bucket': 'zyyxpachong'
+}
+
 news = {
     'db': {
         'host': '192.168.0.53',
@@ -37,17 +53,3 @@ fund_notice = {
         'timeout': 60
     }
 }
-
-ufile = {
-    'public_key': 'ucloudgcqin@go-goal.com13648682571239575500',
-    'private_key': '27f435a8c39f515b01a3db66acbdd7ef9b37d16c',
-    'bucket': 'zyyxpachong'
-}
-
-DEFAULT_REQUEST_HEADERS = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'
-}
-
-proxy = 'http://111.13.111.184:80'
