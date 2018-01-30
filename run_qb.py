cbfe1379d045821024cb3ec277169c91d524bac9
qianbin <qianbin@go-goal.com>
2018-01-30 18:12:24 +0800

    ......

--- run_qb.py
+++ run_qb.py
@@ -1,5 +1,5 @@
 from scrapy import cmdline
 
-cmdline.execute(['scrapy', 'crawl', 'FundNav_WangXinzq', '-a', 'jobId=0L'])
-# cmdline.execute(['scrapy', 'crawl', 'FundNav_ZhongTieturst', '-a', 'jobId=0L'])
+# cmdline.execute(['scrapy', 'crawl', 'FundNav_WangXinzq', '-a', 'jobId=0L'])
+cmdline.execute(['scrapy', 'crawl', 'FundNav_ZhongTieturst', '-a', 'jobId=0L'])
 
