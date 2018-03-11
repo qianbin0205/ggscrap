1d1967212f6c0d36b4375d33681328adcb0cc48c
qianbin <qianbin0205@163.com>
2018-03-11 23:36:03 +0800

    ......

---
+++ run.py
@@ -0,0 +1,21 @@
+from scrapy import cmdline
+
+# free
+# cmdline.execute(['scrapy', 'crawl', 'FundNav_WangXinzq', '-a', 'jobId=0L'])
+# cmdline.execute(['scrapy', 'crawl', 'FundNav_ZhongTieturst', '-a', 'jobId=0L'])
+# cmdline.execute(['scrapy', 'crawl', 'FundNav_TengBeiErAsset', '-a', 'jobId=0L'])
+# cmdline.execute(['scrapy', 'crawl', 'FundNav_ShanXiSecurities', '-a', 'jobId=0L'])
+
+# cookie
+# cmdline.execute(['scrapy', 'crawl', 'FundNav_XingJuInvest', '-a', 'jobId=0L'])
+# cmdline.execute(['scrapy', 'crawl', 'FundNav_ShiTianFengInvest', '-a', 'jobId=0L'])
+cmdline.execute(['scrapy', 'crawl', 'FundNav_TengYeCapitals', '-a', 'jobId=0L'])
+
+# log_in
+# cmdline.execute(['scrapy', 'crawl', 'FundNav_XingRongBangAsset', '-a', 'jobId=0L'])
+# cmdline.execute(['scrapy', 'crawl', 'FundNav_ZhongJiangRongXinInvest', '-a', 'jobId=0L'])
+# cmdline.execute(['scrapy', 'crawl', 'FundNav_ChengShiTianXiaInvest', '-a', 'jobId=0L'])
+
+
+
+
